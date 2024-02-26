e913e6acc066ddbcbf57fdc3348260f3e4f457ac
Dominic D <dominic.l.dimarco@gmail.com>
2024-02-26 09:32:24 -0700

    Adds typing animation to login, updates style slightly

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/login.css'; // Import the login.css file
+import '../styles/login.css';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [typingHeader, setTypingHeader] = useState('');
+  //const [typingUsername, setTypingUsername] = useState(false);
   const navigate = useNavigate();
+
+  const intervalIdRef = useRef(null);
 
   const handleLogin = () => {
     // Simulate a successful login (replace this with your actual authentication logic)
@@ -18,10 +22,56 @@ const Login = () => {
     }
   };
 
+  const typeWritingPortfolio = () => {
+    const text = "Writing Portfolio";
+    let currentIndex = 0;
+  
+    intervalIdRef.current = setInterval(() => {
+      if (currentIndex < (text.length - 1)) {
+        setTypingHeader((prev) => {
+          const updatedHeader = prev + text[currentIndex];
+          console.log("Updated Header:", updatedHeader);
+          return updatedHeader;
+        });
+        currentIndex++;
+      } else {
+        clearInterval(intervalIdRef.current);
+        setTimeout(backspaceWritingPortfolio, 1000);
+      }
+    }, 100);
+  };
+  
+  const backspaceWritingPortfolio = () => {
+    let text = "Writing Portfolio";
+    let currentIndex = text.length;
+  
+    intervalIdRef.current = setInterval(() => {
+      if (currentIndex > 0) {
+        setTypingHeader((prev) => {
+          const updatedHeader = prev.slice(0, currentIndex - 1);
+          console.log("Updated Header:", updatedHeader);
+          return updatedHeader;
+        });
+        currentIndex--;
+      } else {
+        clearInterval(intervalIdRef.current);
+        setTypingHeader(''); // Clear the typingHeader after backspace
+      }
+    }, 100);
+  };
+  
+  useEffect(() => {
+    typeWritingPortfolio();
+    return () => clearInterval(intervalIdRef.current); // Clear the interval on component unmount
+  }, [/* add dependencies here if needed */]);
+  
+
   return (
-    <div className="login-container" data-testid="login-component"> {/* Apply a class for styling */}
+    <div className="login-container" data-testid="login-component">
       <form>
-        <h2>Login</h2>
+        <h2>
+          <span className="typing-animation" style={{ whiteSpace: 'pre-line' }}>{typingHeader}</span>
+        </h2>
         <label>
           Username:
           <input
